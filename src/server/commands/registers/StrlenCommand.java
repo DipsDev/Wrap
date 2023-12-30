6a07541aa69acccbf5ad78ed25851b2105a0fbf5
1Dips <ido.geva.b@gmail.com>
2023-12-30 19:17:22 +0200

    Added strlen command, a command that returns the length of a string key

---
+++ src/server/commands/registers/StrlenCommand.java
@@ -0,0 +1,36 @@
+package server.commands.registers;
+
+import server.WrapDB;
+import server.commands.Command;
+import server.commands.RegisteredCommand;
+import server.models.datatypes.DataType;
+import server.models.datatypes.Integer;
+import server.models.datatypes.Null;
+import server.models.datatypes.SimpleError;
+import server.models.storetypes.StoreType;
+import server.models.storetypes.StringStore;
+
+@RegisteredCommand
+public class StrlenCommand implements Command {
+    @Override
+    public String getName() {
+        return "strlen";
+    }
+
+    @Override
+    public int getArgsCount() {
+        return 1;
+    }
+
+    @Override
+    public DataType execute(String[] args) {
+        StoreType data = WrapDB.getInstance().get(args[1]);
+        if (data == null) {
+            return new Null();
+        }
+        if (!(data instanceof StringStore)) {
+            return new SimpleError("ERR Get command only supports strings");
+        }
+        return new Integer(((StringStore) data).get("").length());
+    }
+}
